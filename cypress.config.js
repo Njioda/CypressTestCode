cdfb2187d4df4f0775d3551310709706ec10749e
nnjioda@yahoo.fr <nnjioda@yahoo.fr>
2025-08-15 14:52:06 +0200

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: true,
+  reporter: "cypress-mochawesome-reporter",
+  e2e: {
+    baseUrl: "https://demoblaze.com/",
+    excludeSpecPattern: ["**/1-getting-started", "**/2-advanced-examples"],
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
+    }
+  }
+});
